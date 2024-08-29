c07dbbcdb115b8c6cd3ea1b67c2e7dc5bd132742
Nichlas Bo Christensen <NBC@energinet.dk>
2024-08-29 08:41:49 +0200

    k

--- db.ps1
+++ db.ps1
@@ -1,9 +1,16 @@
 # Delete all local branches named "feature/*" that have been merged into master
 
 # Write output to console 
+# Delete all local branches named "feature/*" that have been merged into master
+
+# Write output to console
 git checkout master
 git pull
-git branch --merged `
-| Where-Object { $_.Trim() -like "features/*" } `
-| ForEach-Object { git branch -d $_.Trim() }
-git fetch --prune
+git fetch -p
+git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | ForEach-Object {
+    $branch = $_.Trim().Split(' ')[0]
+    $upstream = $_.Trim().Split(' ')[1]
+    if ($upstream -eq "[gone]") {
+        git branch -D $branch.replace("refs/heads/", "")
+    }
+}
